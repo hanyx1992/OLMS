9362d1ebf5598dd3ecff293b11e88dbd6fed1dee
hanyx1992@sina.com <hanyx1992@sina.com>
2016-01-05 18:24:01 +0800

    搭建工程

---
+++ src/com/chenlu/olms/controller/ExplorerController.java
@@ -0,0 +1,15 @@
+package com.chenlu.olms.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExplorerController {
+
+	@RequestMapping(value = "/main.do")
+	public String queryAll(HttpServletRequest request) {
+		return "main";
+	}
+}
